7ca401236f04c34761dde931b483c289c244aa27
Ryan@CS <ryancs@Yang-Ryans-MacBook.local>
2018-09-20 14:28:28 -0700

    snapshot

---
+++ src/app.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('#btnCsx').click(function () {
+    window.open('https://csx.codesmith.io/', '_blank');
+  });
+  $('#btnGhub').click(function () {
+    window.open('https://github.com/', '_blank');
+  });
+  $('#btnMdn').click(function () {
+    window.open('https://developer.mozilla.org/en-US/', '_blank');
+  });
+});
+
